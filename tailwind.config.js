81739c4ac38b55fdd71e2de91481be347e06ce4b
tuilakhoa <tuilakhoa@github.com>
2026-08-28 18:25:49 +0700

    Fix: Compile Tailwind CSS to fix broken layout caused by disabled CDN

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./themes/**/*.php",
+    "./includes/**/*.php",
+    "./assets/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
